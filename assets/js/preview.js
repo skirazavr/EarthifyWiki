a3e8288acb45dbb0975d4fabc8aaf1fefb72da6f
skirazavr <skirazavr@gmail.com>
2026-08-05 00:34:01 +0300

    Update preview.js

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cache = new Map();
 
     document.querySelectorAll(
-        '.mob-card, .main a[href$=""]'
+        '.mob-card, .main a'
     ).forEach(card => {
 
         card.addEventListener("mouseenter", async () => {
